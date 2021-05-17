9f59b6fdfbd88626ec45cae81e2833e16f2462b2
Miloš Havlíček <miloshavlicek@gmail.com>
2021-05-17 18:10:05 +0200

    Added bootstrap, sass, initial content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css"
+import { AppProps } from "next/app";
+import "../styles/globals.scss"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
